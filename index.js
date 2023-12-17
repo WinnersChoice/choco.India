13c623dc32b4ee6407f63d45c6831d03340c3f05
Agrawal Tanmay Shravanbhai <125138805+WinnersChoice@users.noreply.github.com>
2023-12-17 17:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-//  function setCookie(cname, cvalue, exdays) {
-//     const d = new Date();
-//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//     let expires = "expires=" + d.toUTCString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-//   }
+  function setCookie(cname, cvalue, exdays) {
+     const d = new Date();
+     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+   }
   
-//   function getCookie(cname) {
-//     let name = cname + "=";
-//     let decodedCookie = decodeURIComponent(document.cookie);
-//     let ca = decodedCookie.split(';');
-//     for (let i = 0; i < ca.length; i++) {
-//       let c = ca[i];
-//       while (c.charAt(0) == ' ') {
-//         c = c.substring(1);
-//       }
-//       if (c.indexOf(name) == 0) {
-//         return c.substring(name.length, c.length);
-//       }
-//     }
-//     return "";
-//   }
+   function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
   
-//   function checkCookie() {
-//     let user = getCookie("username");
-//     if (user != "") {
-//       let newName = prompt("Your current name is " + user + ". Do you want to change it?", user);
-//       if (newName != null && newName != "") {
-//         setCookie("username", newName, 30);
-//         alert("Welcome " + newName + "! Your name has been updated.");
-//       } else {
-//         alert("Welcome again " + user);
-//       }
-//     } else {
-//       user = prompt("Please enter your name:", "");
-//       if (user != "" && user != null) {
-//         setCookie("username", user, 30);
-//       }
-//     }
-//   }                    
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      let newName = prompt("Your current name is " + user + ". Do you want to change it?", user);
+      if (newName != null && newName != "") {
+        setCookie("username", newName, 30);
+        alert("Welcome " + newName + "! Your name has been updated.");
+      } else {
+        alert("Welcome again " + user);
+      }
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 30);
+      }
+    }
+  }                    
 
 
   function openYouTube(){
